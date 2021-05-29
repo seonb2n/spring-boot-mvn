7079872007c5b77f254a7441ade54f2d4786c503
seonb2n <626ksb@gmail.com>
2021-05-29 17:34:07 +0900

    Logger 활용하는 법 배움

--- src/main/java/com/exmple/SampleRunner.java
+++ src/main/java/com/exmple/SampleRunner.java
@@ -1,5 +1,7 @@
 package com.exmple;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SampleRunner implements ApplicationRunner {
+
+    private Logger logger = LoggerFactory.getLogger(SampleRunner.class);
 
     @Autowired
     private String hello;
@@ -18,9 +22,8 @@ public class SampleRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("======================");
-        System.out.println(hello);
-        System.out.println(sampleProperties.getName());
-        System.out.println(sampleProperties.getFullName());
+        logger.info(sampleProperties.getName());
+        logger.info(sampleProperties.getFullName());
         System.out.println("======================");
 
     }
