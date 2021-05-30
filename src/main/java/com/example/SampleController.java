90bc7c5270ea9f13db053b142d1455df6929c1c1
seonb2n <626ksb@gmail.com>
2021-05-30 13:33:51 +0900

    mockMVC 에서 get 메소드를 보내서 데이터와 주어진 값에 대해 확인하고 출력하도록 함.

--- src/main/java/com/example/SampleController.java
+++ src/main/java/com/example/SampleController.java
@@ -13,6 +13,6 @@ public class SampleController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello" + sampleService.getName();
+        return "hello " + sampleService.getName();
     }
 }
--- src/test/java/com/example/SampleControllerTest.java
+++ src/test/java/com/example/SampleControllerTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,6 +9,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -16,4 +21,12 @@ public class SampleControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception{
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello seonbin"))
+                .andDo(print());
+    }
 }
