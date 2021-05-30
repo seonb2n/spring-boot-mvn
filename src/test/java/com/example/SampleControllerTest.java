adbff8a035dcad336525bde3a401c4cddadd4f0a
seonb2n <626ksb@gmail.com>
2021-05-30 14:21:00 +0900

    webTestClient 를 추가해서 활용. pom.xml 에 의존성 추가.

--- src/test/java/com/example/SampleControllerTest.java
+++ src/test/java/com/example/SampleControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -16,7 +17,7 @@ import static org.mockito.Mockito.when;
 public class SampleControllerTest {
 
     @Autowired
-    TestRestTemplate testRestTemplate;
+    WebTestClient webTestClient;
 
     @MockBean
     SampleService mockSampleService;
@@ -24,7 +25,9 @@ public class SampleControllerTest {
     @Test
     public void hello() throws Exception{
         when(mockSampleService.getName()).thenReturn("seonbin");
-        String result = testRestTemplate.getForObject("/hello",String.class);
-        assertThat(result).isEqualTo("hello seonbin");
+
+        webTestClient.get().uri("/hello").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("hello seonbin");
     }
 }
