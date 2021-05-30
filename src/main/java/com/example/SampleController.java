b4f2808359b2dac9183951dfe92a2db256670dac
seonb2n <626ksb@gmail.com>
2021-05-30 13:26:50 +0900

    간편하게 test 하고자 mockMVC 추가

---
+++ src/main/java/com/example/SampleController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @Autowired
+    private SampleService sampleService;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello" + sampleService.getName();
+    }
+}
---
+++ src/test/java/com/example/SampleControllerTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+}
