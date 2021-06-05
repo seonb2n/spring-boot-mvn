97eb84e0f3968aa31df400fbc5ee347e49c47bce
seonb2n <626ksb@gmail.com>
2021-06-05 17:07:36 +0900

    error handler 를 만들어서, error 발생 시 처리 겨로가 확인

---
+++ src/main/java/me/seonbin/demospringmvc/SampleException.java
@@ -0,0 +1,5 @@
+package me.seonbin.demospringmvc;
+
+public class SampleException extends RuntimeException {
+
+}
--- src/main/java/me/seonbin/demospringmvc/SampleTemplateController.java
+++ src/main/java/me/seonbin/demospringmvc/SampleTemplateController.java
@@ -1,9 +1,12 @@
 package me.seonbin.demospringmvc;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleTemplateController {
@@ -15,6 +18,21 @@ public class SampleTemplateController {
         model.addAttribute("name","seonbin");
         return "template";
     }
+
+    @GetMapping("/error")
+    public String error() {
+        throw new SampleException();
+    }
+
+    //Exception 이 발생하고, Exception handler 로 아래 거가 사용됨됨
+   @ExceptionHandler(SampleException.class)
+    public @ResponseBody AppError sampleError(SampleException e) {
+        AppError appError = new AppError();
+        appError.setMessage("error.app.key");
+        appError.setReason("Idont know");
+        return appError;
+    }
+
 
 
 }
